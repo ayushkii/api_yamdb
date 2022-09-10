2950cb92f5e37b966e52763e3a383ea7bb1df9bb
Андрей Крылов <numinga27@yandex.ru>
2022-09-10 22:57:18 +0400

    заливка

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_yasg',
 
+
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .models import Category, Comment, Genre, TitleGenre, Reviews, Title, User
 
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+
 admin.site.register(Title)
 admin.site.register(Genre)
 admin.site.register(TitleGenre)
