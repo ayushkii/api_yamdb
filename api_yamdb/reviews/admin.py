055f225f237e1ceeb6675b1514ee0fa57e91fa00
Андрей Крылов <numinga27@yandex.ru>
2022-09-10 20:58:23 +0400

    122

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from reviews.models import Category, Genre, Title, TitleGenre
 
 
+
 admin.site.register(Title)
 admin.site.register(Genre)
 admin.site.register(TitleGenre)
