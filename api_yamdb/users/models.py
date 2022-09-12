54fdc7430fe87f1ed6da02ef14a0b0c708b0fa52
ayushkii <102290437+ayushkii@users.noreply.github.com>
2022-09-12 13:01:05 +0300

    Update models.py

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+USER = 'user'
+MOD = 'moderator'
+ADMIN = 'admin'
+ROLE_CHOICES = [
+    (USER, 'user'),
+    (MOD, 'moderator'),
+    (ADMIN, 'admin'),
+]
+
 
 class User(AbstractUser):
     bio = models.TextField(
         'Биография',
         blank=True,
     )
-    role = models.TextField(
+    role = models.CharField(
         'Роль',
-        default='user',
+        default=USER,
+        choices=ROLE_CHOICES,
+        max_length=10
     )
 
 
